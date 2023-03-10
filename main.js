fa871e3c221dec37e9464a12b01dda6583d7db23
agusting21 <agustin.gomez.a2@gmail.com>
2023-03-10 20:08:54 -0300

    10/03

--- main.js
+++ main.js
@@ -2,16 +2,57 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamicon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoICon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
-menuHamicon.addEventListener('click',toggleDesktopMenu1);
+menuHamicon.addEventListener('click',toggleMobileMenu);
+menuCarritoICon.addEventListener('click',toggleCarritoAside);
+
 
 function toggleDesktopMenu() {
+    const isasideClosed = aside.classList.contains('inactive');
+  
+    if (!isasideClosed) {
+        aside.classList.add('inactive'); // si esta abierto el aside y abro destok se cierra
+
+    }
+
+
     desktopMenu.classList.toggle('inactive')
 
 }
 
-function toggleDesktopMenu1() {
+function toggleMobileMenu() {
+    const isDestokMenuClosed = desktopMenu.classList.contains('inactive');
+  
+    if (!isDestokMenuClosed) {
+        aside.classList.add('inactive');
+
+    }
+
     mobileMenu.classList.toggle('inactive')
 
+}
+
+function toggleCarritoAside() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+  
+        if (!isMobileMenuClosed) {
+            mobileMenu.classList.add('inactive');
+
+        }
+         aside.classList.toggle('inactive') ;
+}
+
+function toggleMobileMenu() {
+
+    const isasideClosed = aside.classList.contains('inactive');
+  
+        if (!isasideClosed) {
+            aside.classList.add('inactive');
+
+        }
+         mobileMenu.classList.toggle('inactive') ;
 }
